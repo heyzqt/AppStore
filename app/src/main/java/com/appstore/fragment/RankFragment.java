692d5e290989f128d766356fecdb09fb2cd8bfeb
erque <1062631582@qq.com>
2016-09-21 10:13:58 +0800

    RankFragment_update

--- app/src/main/java/com/appstore/fragment/RankFragment.java
+++ app/src/main/java/com/appstore/fragment/RankFragment.java
@@ -1,5 +1,6 @@
 package com.appstore.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -17,6 +18,16 @@ public class RankFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.fragment_rank, null);
+        findView(contentView);
         return contentView;
+    }
+
+    private void findView(View contentView) {
+//        TagView tv = (TagView) getView().findViewById(R.id.tags_view);
+//        TagView.Tag[] tags = {
+//                new TagView.Tag("Sample tag", Color.parseColor("#0099CC")),
+//                new TagView.Tag("Another one", Color.parseColor("#9933CC"))
+//        };
+//        tv.setTags(tags, " ");
     }
 }
